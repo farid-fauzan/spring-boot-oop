eaee5319e6b00e750da240e6b5fdab0ef3e4f01f
Bonie <bonie.wijayakadar@gmail.com>
2022-08-02 08:05:34 +0700

    fix

--- src/main/java/bc/casemgt/restapi/controller/DeleteController.java
+++ src/main/java/bc/casemgt/restapi/controller/DeleteController.java
@@ -5,13 +5,15 @@ import bc.casemgt.restapi.services.DeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 
-@RestController("/delete")
+@RestController
+@RequestMapping("/case-management/delete")
 public class DeleteController {
     @Autowired
     private DeleteService deleteService;
